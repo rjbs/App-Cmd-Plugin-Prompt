91b4a4040b9311ac47584a083fb2b55a107b5889
Ricardo SIGNES <rjbs@cpan.org>
2008-12-23 15:16:35 -0500

    import from Pobox

--- lib/App/Cmd/Plugin/Prompt.pm
+++ lib/App/Cmd/Plugin/Prompt.pm
@@ -2,4 +2,143 @@ use strict;
 use warnings;
 package App::Cmd::Plugin::Prompt;
 
+=head1 SUBROUTINES
+
+=head2 prompt_str
+
+  my $input = prompt_str($prompt, \%opt)
+
+This prompts a user for string input.  It can be directed to
+persist until input is 'acceptable'.
+
+Valid options are:
+
+=over 4
+
+=item *
+
+B<input:> optional coderef, which, when invoked, returns the
+user's response; default is to read from STDIN.
+
+=item *
+
+B<output:> optional coderef, which, when invoked (with two
+arguments: the prompt and the choices/default), should
+prompt the user; default is to write to STDOUT.
+
+=item *
+
+B<valid:> an optional coderef which any input is passed into
+and which must return true in order for the program to
+continue
+
+=item *
+
+B<default:> may be any string; must pass the 'valid' coderef
+(if given)
+
+=item *
+
+B<choices:> what to display after the prompt; default is
+either the 'default' parameter or nothing
+
+=item *
+
+B<no_valid_default:> do not test the 'default' parameter
+against the 'valid' coderef
+
+=item *
+
+B<invalid_default_error:> error message to throw when the
+'default' parameter is not valid (does not pass the 'valid'
+coderef)
+
+=back
+
+=cut
+
+sub prompt_str {
+  my ($message, $opt) = @_;
+  if ($opt->{default} && $opt->{valid} && ! $opt->{no_valid_default}) {
+    X::BadValue->throw(
+      $opt->{invalid_default_error} ||
+        "'default' must pass 'valid' parameter"
+      ) unless $opt->{valid}->($opt->{default});
+  }
+  $opt->{input}  ||= sub { scalar <STDIN> };
+  $opt->{output} ||= sub { printf "%s [%s]: ", @_ };
+  $opt->{valid}  ||= sub { 1 };
+
+  my $response;
+  while (!defined($response) || !$opt->{valid}->($response)) {
+    $opt->{output}->(
+      $message,
+      ($opt->{choices} || $opt->{default} || ""),
+    );
+    $response = $opt->{input}->();
+    chomp($response);
+    if ($opt->{default} && ! length($response)) {
+      $response = $opt->{default};
+    }
+  }
+  return $response;
+}
+
+=head2 prompt_yn
+
+  my $bool = prompt_yn($prompt, \%opt);
+
+This prompts the user for a yes or no response and won't give up until it gets
+one.  It returns true for yes and false for no.
+
+Valid options are:
+
+ default: may be yes or no, indicating how to interpret an empty response;
+          if empty, require an explicit answer; defaults to empty
+
+=cut
+
+sub prompt_yn {
+  my ($message, $opt) = @_;
+  X::BadValue->throw("default must be y or n")
+    if $opt->{default}
+    and $opt->{default} ne 'y'
+    and $opt->{default} ne 'n';
+
+  my $choices = (not defined $opt->{default}) ? 'y/n'
+              : $opt->{default} eq 'y'        ? 'Y/n'
+              :                                 'y/N';
+
+  my $response = prompt_str(
+    $message,
+    {
+      choices => $choices,
+      valid   => sub { lc($_[0]) eq 'y' || lc($_[0]) eq 'n' },
+      %$opt,
+    },
+  );
+
+  return lc($response) eq 'y';
+}
+
+=head2 prompt_any_key($prompt)
+
+  my $input = prompt_any_key($prompt);
+
+This routine prompts the user to "press any key to continue."  (C<$prompt>, if
+supplied, is the text to prompt with.
+
+=cut
+
+sub prompt_any_key {
+  my ($prompt) = @_;
+
+  $prompt ||= "press any key to continue";
+  print $prompt;
+  Term::ReadKey::ReadMode 'cbreak';
+  Term::ReadKey::ReadKey(0);
+  Term::ReadKey::ReadMode 'normal';
+  print "\n";
+}
+
 1;
